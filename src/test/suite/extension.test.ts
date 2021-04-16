dc121aeda357b30d0506b5110d6f4575dc162b19
Gary Holt <gholt@google.com>
2021-04-15 22:57:24 -0400

    Support markdown block quotes.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -212,4 +212,14 @@ A grandiose topic
      */`, done);
   });
 
+
+  test('markdown block quote', (done) => {
+    reflowDocument(`
+  > Great quote from some
+  > hallowed piece of literature.
+  `, 1, `
+  > Great quote from some hallowed piece of literature.
+  `, done);
+  });
+
 });
